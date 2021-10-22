482761f3a1e54c2ddd6b777165b6911d7a2876ce
Phạm Ngọc Tâm <90249100+tamnpham@users.noreply.github.com>
2021-10-22 13:27:40 +0700

    mail

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [UsersModule, TasksModule, TypeOrmModule.forRoot(), AuthModule],
+  imports: [UsersModule, TasksModule, TypeOrmModule.forRoot(), AuthModule, MailModule],
   controllers: [AppController],
   providers: [AppService],
 })
